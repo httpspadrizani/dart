9428e2f3d54c16b9191863437124cedda8e63216
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2024-05-03 09:43:21 -0300

    salvando

---
+++ atividade1.dart
@@ -0,0 +1,15 @@
+void main() {
+  int nota1 = 9;
+  int nota2 = 8;
+  int nota3 = 7;
+  int nota4 = 8;
+
+  double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+  // Usando um loop for para verificar se a média é maior ou igual a 7
+  for (int i = 0; i < 1; i++) {
+    if (media >= 7) {
+      print("Você foi aprovado");
+    }
+  }
+}
---
+++ exemplolinguagem.dart
@@ -0,0 +1,19 @@
+void main(){
+
+String linguagem = "PHP";
+
+switch (linguagem) {
+case "HTML":
+  print ("Linguagem de programação legal");
+  break;
+case "PHP":
+  print ("Linguagem de programação super legal");
+  break;
+case "JavaScript":
+  print ("Super legal");
+  break;
+default:
+print ("Não conheço essa linguagem de programação");
+break;
+  }
+}
